eaff615e527c542872a23fb782915c787a965991
Tadas <tadaslaiskonis@gmail.com>
2023-04-28 00:13:24 +0300

    Bugfix

--- Code/views.py
+++ Code/views.py
@@ -6,6 +6,8 @@ def remove_letter(request):
         original_string = request.GET.get('string')
         letter_to_remove = request.GET.get('letter')
         if original_string and letter_to_remove:
+            if len(letter_to_remove) > 1:
+                return JsonResponse("Enter a single letter.", safe=False)
             letter_to_remove_upper = letter_to_remove.upper()
             letter_to_remove_lower = letter_to_remove.lower()
             updated_string = original_string.replace(letter_to_remove_upper, '')
