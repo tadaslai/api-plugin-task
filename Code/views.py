802b9db3d25693e0fee164a08e7d191b61c1fd5b
Tadas <tadaslaiskonis@gmail.com>
2023-04-27 22:10:46 +0300

    Bugfix

--- Code/views.py
+++ Code/views.py
@@ -6,7 +6,11 @@ def remove_letter(request):
         original_string = request.GET.get('string')
         letter_to_remove = request.GET.get('letter')
         if original_string and letter_to_remove:
-            updated_string = original_string.replace(letter_to_remove, '')
+            letter_to_remove_upper = letter_to_remove.upper()
+            letter_to_remove_lower = letter_to_remove.lower()
+            updated_string = original_string.replace(letter_to_remove_upper, '')
+            updated_string = updated_string.replace(letter_to_remove_lower, '')
+
             return JsonResponse(updated_string, safe=False)
         return JsonResponse("A string or a letter is missing from request.", safe=False)
 
